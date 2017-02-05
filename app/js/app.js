d04075d5e4324442996b955035c244a2fa420d03
Stanislav Blokh <s.e.blokh@gmail.com>
2017-02-05 15:40:39 +0200

    no message

---
+++ app/js/app.js
@@ -0,0 +1,5 @@
+var app = angular.module('myApp', [])
+
+app.controller('tableCtr', function(){
+			   
+			   });
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,32 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglifyjs'),
+	reload = require('browser-sync'),
+	concat = require('gulp-concat');
+
+gulp.task('sass', function(){ // Создаем таск Sass
+	return gulp.src('app/sass/**/*.sass') // Берем источник
+		.pipe(sass())
+		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+		.pipe(reload.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+
+gulp.task('browser-sync', function () {  // Создаем таск browser-sync
+
+	reload({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	})
+});
+	
+
+gulp.task('watch', ['browser-sync'], function() {
+	gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+	gulp.watch('app/*.html', reload.reload); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch('app/js/**/*.js', reload.reload);   // Наблюдение за JS файлами в папке js
+});
+
+gulp.task('default', ['watch']);
