18cd20e569e41ee9995d98892b3326baaba3dd44
Stanislav Blokh <s.e.blokh@gmail.com>
2017-02-05 23:08:12 +0200

    рабочая таблица, сервер через $http.get и api.myjson

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,27 @@
-var app = angular.module('myApp', [])
+var app = angular.module('myApp', ['ngResource', 'ngRoute'])
 
-app.controller('tableCtr', function(){
-			   
-			   });
+var URL='https://api.myjson.com/bins/lqh89'
+
+app.controller('tableCtr', function($http, $scope){
+ 
+	 $http.get(URL).success(function(data){
+		 $scope.userData = data
+	 })
+});
+
+/*
+app.factory('UserData', function($resource) {
+
+	return $resource(URL);
+
+});
+
+*/
+
+app.filter("yesFilter", function () {
+	return function (input) {
+
+		if (input) return '\u2713'
+		else return '\u2718';
+	}
+});
